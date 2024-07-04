4598490e27f41d1f55a127a74d3765de0102a6e8
Jake <86923926+Jake2580@users.noreply.github.com>
2024-07-04 12:26:44 +0900

    First commit.

---
+++ routes/real-estate.js
@@ -0,0 +1,29 @@
+let router = require('express').Router();
+
+// BackendServer real-estate
+// 부동산 매물 등록 Submit
+router.post('/save', async function (req, res) {
+    //
+});
+
+// 부동산 매물 수정 Submit
+router.post('/edit', async function (req, res) {
+    //
+});
+
+// 부동산 매물 삭제 Submit
+router.post('/delete', async function (req, res) {
+    //
+});
+
+// 부동산 매물 매매가 구매 Submit
+router.post('/selling', async function (req, res) {
+    //
+});
+
+// 부동산 매물 전세가 구매 Submit
+router.post('/jeonse', async function (req, res) {
+    //
+});
+
+module.exports = router;
