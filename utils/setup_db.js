4b2f3f45f9fb765a9a595d5c548290a1cbacf3b3
Jake <86923926+Jake2580@users.noreply.github.com>
2024-08-04 01:19:42 +0900

    Do you know the environment variable that defines the name of the MySQL database? Its name has been changed.

--- utils/setup_db.js
+++ utils/setup_db.js
@@ -10,7 +10,7 @@ const setup = async () => {
         mysqldb = mysql.createConnection({
             host: process.env.MYSQL_HOST,
             port: process.env.MYSQL_PORT,
-            database: process.env.MYSQL_DB,
+            database: process.env.MYSQL_DATABASE,
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASSWORD,
         });
